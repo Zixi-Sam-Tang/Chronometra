47b9e9854e543b503e79646a390d06b9d562986c
sjy-chen <samueljychen@gmail.com>
2020-06-08 20:40:00 -0400

    Finished_Entry_Room

--- Tilesets/Stone.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="roguelikeDungeon_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="522" columns="29">
- <image source="../../../../Downloads/Roguelike Cave pack/Spritesheet/roguelikeDungeon_transparent.png" width="492" height="305"/>
-</tileset>
--- Tilesets/roguelikeDungeon_transparent.tsx
+++ Tilesets/roguelikeDungeon_transparent.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" margin="1" tilecount="1920" columns="60">
- <image source="../../../../Downloads/Roguelike pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+<tileset version="1.2" tiledversion="1.3.5" name="roguelikeDungeon_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="522" columns="29">
+ <image source="roguelikeDungeon_transparent.png" trans="ff00ff" width="492" height="305"/>
 </tileset>
