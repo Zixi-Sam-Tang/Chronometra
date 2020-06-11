00e90590ec58e166fa3b739b45d9301ebefe8f94
Zixi Tang <zixi.sam.tang@gmail.com>
2020-06-11 11:42:26 -0400

    Chronometra V0.2

--- main game loop.py
+++ main game loop.py
@@ -31,6 +31,8 @@ class fireball(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.vel = 10
+
     def draw(self, win):
         win.blit(pygame.image.load('Images\Fire Ball Right.png'), (self.x, self.y))
 
@@ -39,33 +41,36 @@ class heart(object):
         self.x = x
         self.y = y
         self.vel = 7
+        self.rage = 0
     def draw(self, win):
         win.blit(pygame.image.load('Images\Heart.png'), (self.x, self.y))
+        #health bar
+        pygame.draw.rect(win, (255, 0, 0), (10, 400, 100, 20))
+        pygame.draw.rect(win, (0, 255, 0), (10 + self.rage, 400, 100 - self.rage, 20))
+
+    def hit(self):
+        for fb in fireballs:
+            if fb.x >= h.x - 16 and fb.x <= h.x + 16 and fb.y >= h.y - 16 and fb.y <= h.y + 16:
+                fireballs.pop(fireballs.index(fb))
+                self.rage += 5
 
 
-def bf(stage):
-    if stage == 1:
-        for i in range(0, 10):
-            y = random.randint(470, 647)
-            fireballs.append(fireball(10, y))
-        stage += 1
 
 def redrawGameWindow():
     p.draw(win)
-
-
     if bossfight:
         win.blit(pygame.image.load('Images\Boss Fight Screen.png'), (0, 0))
         win.blit(pygame.image.load('Images\WrathDemon.png'), (275, 0))
         h.draw(win)
         for fb in fireballs:
             fb.draw(win)
-
     pygame.display.update()
 
 
-
+global FPS
+FPS = 60
 bossfight = True
+temp = 0
 global fireballs
 fireballs = []
 stage = 1
@@ -75,7 +80,7 @@ h = heart(384, 574)
 running = True
 while running:
 
-
+    tick = pygame.time.get_ticks()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -101,16 +106,30 @@ while running:
         p.move = "w"
     elif keys[pygame.K_w] and bossfight and h.y >= 480:
         h.y -= h.vel
+
     if bossfight and stage <= 3:
         p.move = "hidden"
-        bf(stage)
-        stage += 1
+        if stage == 1:
+            y = random.randint(470, 647)
+            fireballs.append(fireball(10, y))
+            temp += 1
+            if temp == 7:
+                stage += 1
+                temp = 0
+        if stage == 2:
+            y = random.randint(470, 647)
+            fireballs.append(fireball(10, y))
+            temp += 1
+            if temp == 50:
+                stage += 1
+                temp = 0
 
     for fb in fireballs:
         if fb.x > 0 and fb.x < 800:
-            fb.x += 10
+            fb.x += fb.vel
         else:
             fireballs.pop(fireballs.index(fb))
+    h.hit()
     win.fill((0, 0, 0))
     redrawGameWindow()
-    pygame.display.update()
+pygame.quit()
