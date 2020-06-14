d288833c8ee9beaf1bbb15331f63e6f09972eed4
Zixi Tang <zixi.sam.tang@gmail.com>
2020-06-14 18:41:25 -0400

    Chronometra V1.0

---
+++ Tilesets/Lava.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="21_subrosiatiles" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="493" columns="17">
+ <image source="21_subrosiatiles.png" width="290" height="494"/>
+</tileset>
